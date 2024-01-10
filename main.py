95cfb2a93c317e9d6d8742048f01a80696cfb3e7
John Mwaniki <mwaniki.john@s.karu.ac.ke>
2024-01-10 10:04:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class FizzBuzz:
-    def fizz(self,x):
-        for i in range(1,x+1):
-            if i%3 == 0:
+    def fizz(self, x):
+        for i in range(1, x + 1):
+            if i % 3 == 0:
                 print("Fizz")
-            elif i%5 == 0:
+            elif i % 5 == 0:
                 print("Buzz")
-            elif i%3==0 and i%5== 0:
+            elif i % 3==0 and i % 5== 0:
                 print("Fizz Buzz")
             else:
                 print(i)
